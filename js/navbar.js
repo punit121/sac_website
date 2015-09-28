51b773749a20a2c2165c89057142135e14e46f26
Hitesh Saini <saini.hitesh4@gmail.com>
2015-09-28 09:22:39 +0530

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -6,14 +6,16 @@ $(document).ready(function(){
 			$('.navbar').removeClass('');
 			$('.nav').removeClass('navbar-left');
 			$('.nav ').addClass('navbar-right');
-			 $("logo").attr("src", "http://placehold.it/60x60");
+			 $(".logo").attr("src", "http://placehold.it/60x60");
+			
 			
 		}else if(position_top<200){
 			$('.navbar ').removeClass('navbar-fixed-top');
 			$('.navbar ').addClass('navbar-inverse');
 			$('.nav').removeClass('navbar-right');
 			$('.nav ').addClass('navbar-left');
-		 	$("logo").removeAttr("src");
+		 	$(".logo").removeAttr("src");
+		 	
 		}
 	} );
 	
