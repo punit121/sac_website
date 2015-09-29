bda3dd78db08f252957644ac6f5a9f9975e5ae1e
punit121 <punit.kumar1661@gmail.com>
2015-09-29 18:56:11 +0530

    changes done

--- imprint.php
+++ imprint.php
@@ -2,38 +2,38 @@
 <html lang="en">
 <head> 
   <title>Students' Alumni Cell &middot; IIT Kharagpur</title>
-	<?php include 'metatags.php'; ?>
-   <?php include 'css_scripts.php';?>
-   
+   <?php include 'metatags.php'; ?>
+   <?php include 'css_scripts.php';?>  
 </head>
-
 <body >        
-
-
-<div class="container">
-
-<br>
-	<img src="http://placehold.it/100x100" style="float:left"/>
-
+	<div class="container">
+		<br>
+		<?php include 'topheader.php'; ?><br><br>
+		    <?php include 'navbar.php' ;?>
 	
-			<h1 style="text-align:center;font-family:'gill sans MT',arial,sans-serif;">STUDENTS' ALUMNI CELL</h1><br><br>
-<?php include 'navbar.php' ;?>
+			<div >
+				<div  style="padding:10px;margin:10px;" >
+					<a href="http://www.webteam.iitkgp.ernet.in/scg/" style="text-decoration:none;"><h2>My Imprint</h2></a>
+					<h5>
+					IIT-KGP has a long and historic legacy of Alumni Giving Back which has existed in one or the other form.
+					The Graduating Class Gift is yet another chapter to be added in KGP alumni legacy initiated under My Imprint Program.
+					It is a class-based effort directed towards raising unrestricted fund partially used currently and partially going towards Endowment. 
+					This particular part of the Endowment would again be utilized for students' services only. It is an opportunity for graduating students 
+					to take their first step in being soon-to-be alumni by making their Graduating Class Gift, the gift which impacts student life in the KGP
+					campus. This challenge offers seniors the chance not only to make a gift, but also to leave behind their imprint
+					As the My Imprint Program says, "By the students, For the Students", the following 9 options have been formulated by My Imprint Class Gift
+					Committee, comprising of students only. Please click on The Committee tab to see the names of committee members.
+					The 9 options have been categorized in 3 slabs, according to the sign-ups raised by the Class of 2015 at the end of campaigning. 
+					You may make your choice of Class Gift in every slab. So, vote for 3 class gifts, one in each slab.<br><br><br><br><br><br><br><br><br>
+					<a href="http://www.webteam.iitkgp.ernet.in/scg/"  style="text-decoration:none;">Click Here</a> to visit My Imprint page
+					</h5><br><br><br><br><br><br><br><br>
+			    </div>
+				
+			</div>
+		<?php include 'footer.php';?>
+		<?php include 'script.php';?>
 
-<div class="container">
-<div class="row">
-<div class="col-sm-8">
+	</div>
 
-</div>
-<div class="col-sm-4">
-
-</div>
-</div>
-</div>
-
-<?php include 'footer.php';?>
-
-
-</div>
-
- </body>
+</body>
 </html>
--- mentor.php
+++ mentor.php
@@ -1,51 +1,40 @@
+<!DOCTYPE html>
 <html lang="en">
-<head>
-	<head>
-	<title>Students' Alumni Cell &middot; IIT Kharagpur</title>
-		<?php include 'metatags.php'; ?>
-		<?php include 'css_include.php';?>
-	</head>
+<head> 
+  <title>Students' Alumni Cell &middot; IIT Kharagpur</title>
+   <?php include 'metatags.php'; ?>
+   <?php include 'css_include.php';?>  
 </head>
-<body>
-<div class="container">
-		<div class="row" style="position:absolute;right:180px;">	
-	<?php include 'links.php';?>
-	
-	</div>
-	<br><br>	
-	<?php include 'navbar.php' ;?>
-	<br>
-	<div id="wrapper">
-		
-	<div class="container demo-3" style="padding:0px;margin:0px">
-     <div class="col-sm-9" style="padding:0px;margin:0px">
-		<h1 style="font-style:Georgia;text-align:jstify;">Mentorship Programme</h1><hr>
-		The Student Alumni Mentorship programme was started in session 2010-11 by the Students’ Alumni Cell
-		as an initiative to create a platform for the exchange of ideas and experience between the alumni and 
-		the current students of IIT Kharagpur.
-		With an ocean of possibilities available, students often find it difficult to decide their way out
-		into the future. There are lots of instances where students need a mentor who can counsel and guide 
-		them through. The alumni can improve students' insight into various aspects of life. Their journey 
-		and personal experiences can help the students and motivate them through this pivotal phase of life.
-		Under this programme, we invite our alumni from across the world and from different fields to interact
-		with the students and help them build their network and gain new perspective on the possibilities open
-		to them after graduation.	
-		</div>
-	 <div class="col-sm-3"  style="padding:50px;">
-		<button type="button" class="btn btn-default">Apply</button><br>
+<body >        
+	<div class="container">
 		<br>
-		<button type="button" class="btn btn-success">Log In</button>
-	 </div>
-    </div>	
+			<?php include 'topheader.php';?><br><br>
+		    <?php include 'navbar.php' ;?>
 	
-</div>
-</div>	
+			<div >
+				<div  style="padding:10px;margin:10px;" >
+					<a href="http://www.webteam.iitkgp.ernet.in/scg/" style="text-decoration:none;"><h2>Mentorship Programme</h2></a>
+										<h5>The Student Alumni Mentorship programme was started in session 2010-11 by the Students’ Alumni Cell
+					as an initiative to create a platform for the exchange of ideas and experience between the alumni and 
+					the current students of IIT Kharagpur.
+					With an ocean of possibilities available, students often find it difficult to decide their way out
+					into the future. There are lots of instances where students need a mentor who can counsel and guide 
+					them through. The alumni can improve students' insight into various aspects of life. Their journey 
+					and personal experiences can help the students and motivate them through this pivotal phase of life.
+					Under this programme, we invite our alumni from across the world and from different fields to interact
+					with the students and help them build their network and gain new perspective on the possibilities open
+					to them after graduation.
+					<br><br><br><br><br><br><br><br><br>    For more information visit
+                   <a href="http://www.mentorship.iitkgp.ernet.in/mainpage/index" style="text-decoration:none;text-align:center;"> http://www.mentorship.iitkgp.ernet.in/mainpage/index</a>
+				   <br>
+					</h5><br><br><br><br><br><br><br><br>
+			    </div>
+				
+			</div>
+		<?php include 'footer.php';?>
+		<?php include 'js_include.php';?>
 
+	</div>
 
-<script src="js/navbar.js"></script>
-<br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br>
-	<?php include 'footer.php'; ?>
-	<?php include 'js_include.php'; ?>
 </body>
 </html>
