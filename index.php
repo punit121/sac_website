f987dd1a1428f980fa97d34e3813b3547528ece0
hitesh4 <saini.hitesh4@gmail.com>
2015-11-11 04:24:02 +0530

    navbar changed, slider changed

--- index.php
+++ index.php
@@ -4,37 +4,24 @@
 	  <title>Students' Alumni Cell &middot; IIT Kharagpur</title>
 	  <?php include 'metatags.php'; ?>
 	  <?php include 'css_include.php';?>
+	  <style type="text/css">
+
+	  </style>
 	</head>
 	<body >
-	  <div class="wrap">
-	   <div class="container"  >
-	    <?php include 'topheader.php';?>
+	  
+	   <div class="container">
 	    <?php include 'navbar.php' ;?>
-	    <div id="slide">
-		 <div id="sliderFrame">
-		  <div id="slider">
-				<img src="img/indexslider/annualalumnimeet1.jpg" />
-				<img src="img/indexslider/sam4.jpg"/>
-				<img src="img/indexslider/mentorship1.jpg" />
-				<img src="img/indexslider/guest.jpg" />
-				<img src="img/indexslider/givingback.jpg" />
-				<img src="img/indexslider/yoy.jpg" />
-				<img src="img/indexslider/alvida.jpg"/>
-		   </div>
-		</div>
-	</div>
-	<br>
+	    <?php include 'slider1.php' ; ?>
+	
 	<h2 >Objectives </h2>
 	<?php include 'column3.php';?>
 	<h2 >Latest Programs</h2>
 	<?php include 'column4.php' ;?>
 	<?php include 'midbox.php' ;?>	
      </div>
-		<div style="background-image:url('img/vinay.png');width:1315px;background-repeat:no-repeat;margin-left:15px;">
-<?php include 'spons-slider.php' ;?>
-<?php include 'footer.php';?>
-</div>
-</div>
+		
+
 <script src="http://thecodeplayer.com/uploads/js/prefixfree-1.0.7.js" type="text/javascript"></script>
 <?php include 'js_include.php';?>
 	</body>
