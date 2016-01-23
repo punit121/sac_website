9d0f64372b79145dfb39838d2e4b9a563aed338c
Utsav Agarwal <uagarwal744@gmail.com>
2016-01-23 17:37:26 +0530

    Update internalslider.php

--- internalslider.php
+++ internalslider.php
@@ -1,1 +1,103 @@
-<html><head><link rel="stylesheet" href="css/internalslider.css"></head><body><center><div class="slider">	<input type="radio" name="slide_switch" id="id1"/>	<label for="id1">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/1.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/1.JPG" width="600" height="300">		<!--Lets show the second image by default on page load-->	<input type="radio" name="slide_switch" id="id2" checked="checked"/>	<label for="id2">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/2.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/2.JPG" width="600" height="300"/>		<input type="radio" name="slide_switch" id="id3"/>	<label for="id3">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/44.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/44.JPG" width="600" height="300">		<input type="radio" name="slide_switch" id="id4"/>	<label for="id4">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/7.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/7.JPG" width="600" height="300" />		<input type="radio" name="slide_switch" id="id5"/>	<label for="id5">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/9.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/9.JPG" width="600" height="300"  />		<input type="radio" name="slide_switch" id="id6"/>	<label for="id6">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/35.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/35.JPG" width="600" height="300" />		<input type="radio" name="slide_switch" id="id7"/>	<label for="id7">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/50.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/50.JPG" width="600" height="300"/>		<input type="radio" name="slide_switch" id="id8"/>	<label for="id8">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/60.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/60.JPG" width="600" height="300"/>	<input type="radio" name="slide_switch" id="id9"/>	<label for="id9">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/85.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/85.JPG" width="600" height="300"/>	<input type="radio" name="slide_switch" id="id10"/>	<label for="id10">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/63.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/63.JPG" width="600" height="300"/>	<input type="radio" name="slide_switch" id="id11"/>	<label for="id11">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/18.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/18.JPG" width="600" height="300"/>	<input type="radio" name="slide_switch" id="id12"/>	<label for="id12">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/57.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/57.JPG" width="600" height="300"/>	<input type="radio" name="slide_switch" id="id13"/>	<label for="id13">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/66.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/66.JPG"  width="600" height="300"/>	<input type="radio" name="slide_switch" id="id14"/>	<label for="id14">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/22.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/22.JPG" width="600" height="300"/>	<input type="radio" name="slide_switch" id="id15"/>	<label for="id15">		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/15.JPG" width="200"/>	</label>	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/gallery1/15.JPG" width="600" height="300"/></div></center><script src="http://thecodeplayer.com/uploads/js/prefixfree.js" type="text/javascript"></script></body></html>
+<html>
+<head>
+<link rel="stylesheet" href="css/internalslider.css">
+</head>
+<body>
+<center><div class="slider">
+	<input type="radio" name="slide_switch" id="id1"/>
+	<label for="id1">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/1.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/1.jpg" width="600" height="300">
+	
+	<!--Lets show the second image by default on page load-->
+	<input type="radio" name="slide_switch" id="id2" checked="checked"/>
+	<label for="id2">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/2.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/2.jpg" width="600" height="300"/>
+	
+	<input type="radio" name="slide_switch" id="id3"/>
+	<label for="id3">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/44.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/44.jpg" width="600" height="300">
+	
+	<input type="radio" name="slide_switch" id="id4"/>
+	<label for="id4">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/7.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/7.jpg" width="600" height="300" />
+	
+	<input type="radio" name="slide_switch" id="id5"/>
+	<label for="id5">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/9.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/9.jpg" width="600" height="300"  />
+		<input type="radio" name="slide_switch" id="id6"/>
+
+	<label for="id6">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/35.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/35.jpg" width="600" height="300" />
+		<input type="radio" name="slide_switch" id="id7"/>
+
+	<label for="id7">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/50.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/50.jpg" width="600" height="300"/>
+		<input type="radio" name="slide_switch" id="id8"/>
+
+	<label for="id8">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/60.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/60.jpg" width="600" height="300"/>
+	<input type="radio" name="slide_switch" id="id9"/>
+
+	<label for="id9">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/85.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/85.jpg" width="600" height="300"/>
+	<input type="radio" name="slide_switch" id="id10"/>
+
+	<label for="id10">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/63.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/63.jpg" width="600" height="300"/>
+	<input type="radio" name="slide_switch" id="id11"/>
+
+	<label for="id11">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/18.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/18.jpg" width="600" height="300"/>
+	<input type="radio" name="slide_switch" id="id12"/>
+
+	<label for="id12">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/57.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/57.jpg" width="600" height="300"/>
+	<input type="radio" name="slide_switch" id="id13"/>
+
+	<label for="id13">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/66.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/66.jpg"  width="600" height="300"/>
+	<input type="radio" name="slide_switch" id="id14"/>
+
+	<label for="id14">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/22.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/22.jpg" width="600" height="300"/>
+	<input type="radio" name="slide_switch" id="id15"/>
+
+	<label for="id15">
+		<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/15.jpg" width="200"/>
+	</label>
+	<img src="http://alumnimeet.iitkgp.ernet.in/aam-2016/img/meet/15.jpg" width="600" height="300"/>
+</div>
+</center>
+
+<script src="http://thecodeplayer.com/uploads/js/prefixfree.js" type="text/javascript"></script>
+
+</body>
+</html>
